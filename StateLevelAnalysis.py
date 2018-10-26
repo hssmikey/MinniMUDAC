595dc0fde3576ecd6c07e3becc85434fa66ae00a
Jacob <gursky1@stolaf.edu>
2018-10-26 12:55:19 -0500

    Added analysis on a state-level

---
+++ StateLevelAnalysis.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 25 19:19:25 2018
+
+This file is the state-level analysis of voting habits for MN
+
+@author: Jacob
+"""
+
+#Importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Importing our data
+df = pd.read_excel('minnesota-election-statistics-1950-to-2016.xlsx', skiprows = 1, sheetname = 1)
+df.drop(df.tail(1).index,inplace=True)
+
+#Lets create a election type indicator
+df['Presidential'] = np.where(df['Year']%4>0,0,1)
+df['YearSince2014'] = df['Year']-2014
+
+#Lets see a run-sequence plot
+plt.plot(df[df['Presidential']==1]['Year'],df[df['Presidential']==1]['Percent Turnout'])
+plt.plot(df[df['Presidential']==0]['Year'],df[df['Presidential']==0]['Percent Turnout'])
+plt.show()
+
+#Okay, now lets test for autocorrelation
+from pandas.plotting import autocorrelation_plot
+autocorrelation_plot(df['Percent Turnout'])
+#Looks like there is at least 2nd-3rd order autocorrelation on a statewide basis
+
+#Lets try a regular linear regression
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+model.fit(df[['YearSince2014','Presidential']],df['Percent Turnout'])
+print("Coefficients: ", model.coef_.round(5), " Intercept: ", round(model.intercept_,5))
+
+#Lets do this with a LassoCV
+from sklearn.linear_model import LassoCV
+model = LassoCV()
+model.fit(df[['YearSince2014','Presidential']],df['Percent Turnout'])
+print("Coefficients: ", model.coef_.round(5), " Intercept: ", round(model.intercept_,5))
+
+#Lets try an auto-arima
+from pyramid.arima import auto_arima
+stepwise_fit = auto_arima(df['Percent Turnout'].values, 
+                          start_p=1, 
+                          start_q=0, 
+                          max_p=5, 
+                          max_q=0, 
+                          test = 'kpss',
+                          max_d = 2,
+                          seasonal = True,
+                          m=2,
+                          error_action='ignore',
+                          suppress_warnings=True,
+                          stepwise=False,
+                          ic = 'bic')
+stepwise_fit.summary()
+#It seems like this is a 3rd order 
