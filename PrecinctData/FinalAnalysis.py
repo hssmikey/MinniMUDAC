dda18d2499e6609d902ccb79b03cf8c03b3b8622
Jacob <gursky1@stolaf.edu>
2018-11-02 13:26:50 -0500

    Added a correlation csv

--- PrecinctData/FinalAnalysis.py
+++ PrecinctData/FinalAnalysis.py
@@ -47,9 +47,14 @@ rf.fit(X_train, y_train)
 #Showing variable importances
 pd.Series(rf.feature_importances_, index = X_train.columns.tolist()).sort_values(ascending = False)
 
+#Lets test for normality first
+y_train.hist(bins = 20)
+#Bimodal distribution show that linear regression is definitely not appropriate
+
 #Lets look at ridge coefficients
-ridge = RidgeCV(np.linspace(0,10,50),cv = 5)
+ridge = RidgeCV(np.linspace(0,10,50),cv = 10)
 ridge.fit(X_train, y_train)
+ridge.alpha_
 coefs = pd.Series(ridge.coef_, index = X_train.columns.tolist()).sort_values(ascending = False).to_frame()
 coefs['abs'] = coefs[0].abs()
 coefs = coefs.sort_values('abs', ascending = False).drop('abs', axis=1).round(4)
@@ -67,3 +72,54 @@ binomial_results.summary()
 
 #What would our predictions look like?
 binomial_results.predict(X_train)
+
+#Correlation heatmap
+import seaborn as sns
+import matplotlib.pyplot as plt
+def halfHeatMap(df, mirror):
+    # Create Correlation df
+    corr = df.corr()
+    # Plot figsize
+    fig, ax = plt.subplots(figsize=(10, 10))
+    # Generate Color Map
+    colormap = sns.diverging_palette(220, 10, as_cmap=True)
+
+    if mirror == True:
+        #Generate Heat Map, allow annotations and place floats in map
+        sns.heatmap(corr, cmap=colormap, annot=True, fmt=".2f")
+        #Apply xticks
+        plt.xticks(range(len(corr.columns)), corr.columns);
+        #Apply yticks
+        plt.yticks(range(len(corr.columns)), corr.columns)
+        #show plot
+        plt.show()
+    else:
+        # Drop self-correlations
+        dropSelf = np.zeros_like(corr)
+        dropSelf[np.triu_indices_from(dropSelf)] = True# Generate Color Map
+        colormap = sns.diverging_palette(220, 10, as_cmap=True)
+        # Generate Heat Map, allow annotations and place floats in map
+        sns.heatmap(corr, cmap=colormap, annot=True, fmt=".2f", mask=dropSelf)
+        # Apply xticks
+        plt.xticks(range(len(corr.columns)), corr.columns);
+        # Apply yticks
+        plt.yticks(range(len(corr.columns)), corr.columns)
+        #show plot
+        plt.show()
+df_sub = df[[x for x in df.columns.tolist() if not x.startswith('COUNTYNAME_')]]
+halfHeatMap(df_sub, mirror = False)
+
+#Lets just see an array
+df_sub_corr = df_sub.corr()[['Turnout','Turnout_R','Turnout_DFL']]
+df_sub_corr.index = [x.replace('Proportion','Prop')[0:12] for x in df_sub_corr.index.tolist()]
+df_sub_corr
+fig, ax = plt.subplots(figsize=(10, 10))
+colormap = sns.diverging_palette(220, 10, as_cmap=True)
+# Generate Heat Map, allow annotations and place floats in map
+sns.heatmap(df_sub_corr, cmap=colormap, annot=True, fmt=".2f")
+# Apply xticks
+plt.xticks(range(len(df_sub_corr.columns)), df_sub_corr.columns);
+# Apply yticks
+plt.yticks(range(len(df_sub_corr.index)), df_sub_corr.index)
+#show plot
+plt.show()
